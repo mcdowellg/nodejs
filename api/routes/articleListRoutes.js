3bddeb7e4ee42190d6ea82e82fd1957cfe037906
McDowell <Greg.McDowell@pernod-ricard.com>
2019-05-13 13:48:27 +1200

    trying to add this server to an azure cloud

---
+++ api/routes/articleListRoutes.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = function(app) {
+  var articleList = require('../controllers/eventListController');
+  var userHandlers = require('../controllers/userController');
+
+  // articleList Routes
+  app
+  .route("/events")
+  .get(articleList.listAllEvents)
+  .post(articleList.createNewEvent);
+
+  app
+  .route("/event/:eventid")
+  .get(articleList.readArticle)
+  .patch(articleList.updateArticle)
+
+  app
+  .route("/lists")
+  .get(articleList.listAllLists)
+
+  app
+  .route("/gps")
+  .get(articleList.listAllGPS)
+  .post(articleList.createNewGPS);
+
+  // app
+  // .route("/gps/:eventid")
+  // .get(articleList.readArticle)
+  // .patch(articleList.updateArticle)
+
+//   .delete(articleList.deleteArticle);
+
+//   app
+//   .route("/articles/by/:tag")
+//   .get(articleList.listTagArticles);
+
+//   app
+//   .route("/auth/register")
+//   .post(userHandlers.register);
+
+//   app
+//   .route("/auth/sign_in")
+//   .post(userHandlers.signIn);
+
+};
