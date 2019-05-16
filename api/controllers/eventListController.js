8c949e495f3a44b43f3f3b49cec1f3ddef199078
McDowell <Greg.McDowell@pernod-ricard.com>
2019-05-16 18:28:46 +1200

    res direct

--- api/controllers/eventListController.js
+++ api/controllers/eventListController.js
@@ -22,11 +22,11 @@ fs.readFile('../../M10Triplebank.geojson', function(err, Blk){
   res.status(500).send(err);
   }
   console.log(Blk);
-  this.block =  Blk;
+  res.status(200).json(Blk);
   
 })
   var a =  "get this working";
-  res.status(200).json(block);
+  
   
   // Article.$where('this.firstname === this.lastname')
   // Blocks.find({}, (err, article) => {
