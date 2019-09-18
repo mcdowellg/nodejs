f1566fc6b40acf7d672584a54dea0cb1fa9c9ba6
McDowell <Greg.McDowell@pernod-ricard.com>
2019-09-19 09:36:33 +1200

    new api for list updates

--- api/controllers/eventListController.js
+++ api/controllers/eventListController.js
@@ -137,7 +137,7 @@ exports.createNewGPS = (req, res) => {
 
 exports.createNewTask = (req, res) => {
   let newTask = new Tasks()
-  Tasks.save({hi:"please insert"}, (err, event) => {
+  newTask.save({hi:"please insert"}, (err, event) => {
     if (err) {
       res.status(500).send(err);
     }
