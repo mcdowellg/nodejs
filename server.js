7c52d2b9d85ab2a93d76e90715ea88ffd1b22518
McDowell <Greg.McDowell@pernod-ricard.com>
2019-09-23 14:01:52 +1200

    new api for list updates

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '100mb'}));
 
 app.use((req, res, next) => {
     
